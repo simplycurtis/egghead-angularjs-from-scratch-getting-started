c60b6f0e87fb40163eb6d1039f0467dac66ec9e4
Curtis <candrews@cchangeinc.com>
2016-07-27 13:22:57 -0500

    04-simple-states

--- app/eggly-app.working.js
+++ app/eggly-app.working.js
@@ -26,6 +26,9 @@ angular.module('Eggly', [
 
   function setCurrentCategory(category){
     $scope.currentCategory = category;
+
+    cancelCreating();
+    cancelEditing();
   }
 
   function isCurrentCategory(category) {
@@ -34,5 +37,45 @@ angular.module('Eggly', [
 
   $scope.setCurrentCategory = setCurrentCategory; //makes public / available to view
   $scope.isCurrentCategory = isCurrentCategory;
+
+  //-----------------------------------
+  // creating and editing states
+  //-----------------------------------
+
+  $scope.isCreating = false;
+  $scope.isEditing = false;
+
+  function startCreating() {
+    $scope.isCreating = true;
+    $scope.isEditing = false;    
+  }
+
+  function cancelCreating() {
+    $scope.isCreating = false;
+  }
+
+  function startEditing() {
+    $scope.isCreating = false;
+    $scope.isEditing = true;    
+  }
+
+  function cancelEditing() {
+    $scope.isEditing = false;  
+  }
+
+  function shouldShowCreating() {
+    return $scope.currentCategory && !$scope.isEditing;
+  }
+
+  function shouldShowEditing() {
+    return $scope.isEditing && !$scope.isCreating;
+  }
+
+  $scope.startCreating = startCreating;
+  $scope.cancelCreating = cancelCreating;
+  $scope.startEditing = startEditing;
+  $scope.cancelEditing = cancelEditing;
+  $scope.shouldShowCreating = shouldShowCreating;
+  $scope.shouldShowEditing = shouldShowEditing;
 
 });
